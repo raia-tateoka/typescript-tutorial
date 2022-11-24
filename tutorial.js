6d1cefb6665948baaca7fe100e8e308a23504fa4
_gorilla_ <t.workout.r@gmail.com>
2022-11-24 22:22:10 +0900

    Initial commit

---
+++ tutorial.js
@@ -0,0 +1,8 @@
+// const a : Number = 3;
+// const b : Number = a;
+// console.log(b);
+// let array1 : Array<number> = [1,2,3,4];
+var array1 = [1, 2, 3, 4];
+var array2 = [];
+array2 = array1;
+console.log(array2);
